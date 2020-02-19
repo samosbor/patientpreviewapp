7dee8e618a66ea5a583784421a0ed7f9b2e8502d
samosbor <samosbor@byu.edu>
2020-02-19 13:54:46 -0700

    the function works

---
+++ functions/db/db.js
@@ -0,0 +1,12 @@
+var mysql = require('mysql');
+
+var conn = mysql.createConnection({
+  host: 'patientpreview01-mysqldbserver.mysql.database.azure.com',
+	user: 'sam@patientpreview01-mysqldbserver',
+	password: '405admin!',
+	database: 'production',
+	port: 3306,
+	ssl: true
+});
+
+module.exports = conn;
--- functions/xchangeutah/index.js
+++ functions/xchangeutah/index.js
@@ -1,13 +1,32 @@
+var conn = require('../db/db')
+
 module.exports = async function (context, myBlob) {
-    var file = JSON.parse(myBlob.toString())
-
-    file.Default.forEach(element => {
-        var row = fixRow(element)
-
-    });
-
-    context.log("JavaScript blob trigger function processed blob \n Blob:", context.bindingData.blobTrigger, "\n Blob Size:", myBlob.length, "Bytes");
+    try{
+        var file = JSON.parse(myBlob.toString())
+        await testConnection(context)
+        var bulkRows = []
+        file.Default.forEach(element => {
+            var row = fixRow(element)
+            bulkRows.push(row)
+        });
+        let sql = `INSERT INTO testlawsuit(CaseName, CauseOfAction, Plaintiff, Defendant, CaseNumber, DateFiled, Court, PresidingJudge, ReferringJudge, NatureOfSuit, CauseOfActionNumber, JuryDemandedBy, County, State) VALUES ?`
+        conn.query(sql, [bulkRows], (err, results, fields) => {
+            if(err) throw err
+            context.log('Successfully inserted '+ results.affectedRows +'');
+        })
+        context.log("JavaScript blob trigger function processed blob \n Blob:", context.bindingData.blobTrigger, "\n Blob Size:", myBlob.length, "Bytes");
+    } catch(err) {
+        context.log(err)
+    }
 };
+
+async function testConnection(context) {
+    conn.ping(function (err) {
+        if (err)
+            throw err;
+        context.log('Server responded to ping');
+    })
+}
 
 function fixRow(row) {
     var CaseName = row.CaseName
@@ -24,30 +43,42 @@ function fixRow(row) {
     var JuryDemandedBy = ""
     var County = row.County
     var State = "Utah"
+
+    return [CaseName, CauseOfAction, Plaintiff, Defendant, CaseNumber, DateFiled, Court, PresidingJudge, ReferringJudge, NatureOfSuit, CauseOfActionNumber, JuryDemandedBy, County, State]
 }
 
 function getPlaintiff(row) {
     var Plaintiff = ""
     if (row.Plaintiff1 !== "")
         Plaintiff += row.Plaintiff1
-    if (row.Plaintiff2 != "")
+    if (row.Plaintiff2 !== "")
         Plaintiff = Plaintiff + ", " + row.Plaintiff2
-    if (row.Plaintiff3 != "")
+    if (row.Plaintiff3 !== "")
         Plaintiff = Plaintiff + ", " + row.Plaintiff3
-    if (row.Plaintiff4 != "")
+    if (row.Plaintiff4 !== "")
         Plaintiff = Plaintiff + ", " + row.Plaintiff4
-    if (row.Plaintiff5 != "")
+    if (row.Plaintiff5 !== "")
         Plaintiff = Plaintiff + ", " + row.Plaintiff5
-    if (row.Plaintiff6 != "")
+    if (row.Plaintiff6 !== "")
         Plaintiff = Plaintiff + ", " + row.Plaintiff6
-    if (row.Plaintiff7 != "")
+    if (row.Plaintiff7 !== "")
         Plaintiff = Plaintiff + ", " + row.Plaintiff7
-    if (row.Plaintiff8 != "")
+    if (row.Plaintiff8 !== "")
         Plaintiff = Plaintiff + ", " + row.Plaintiff8
-    if (row.Plaintiff9 != "")
+    if (row.Plaintiff9 !== "")
         Plaintiff = Plaintiff + ", " + row.Plaintiff9
-    if (row.Plaintiff10 != "")
+    if (row.Plaintiff10 !== "")
         Plaintiff = Plaintiff + ", " + row.Plaintiff10
+    if (row.Petitioner !== "") {
+        if (Plaintiff !== "")
+            Plaintiff = Plaintiff + ", "
+        Plaintiff = Plaintiff + row.Petitioner
+    }
+    if (row.Petitioner2 !== "") {
+        if (Plaintiff !== "")
+            Plaintiff = Plaintiff + ", "
+        Plaintiff = Plaintiff + row.Petitioner2
+    }
 
     return Plaintiff
 }
@@ -56,23 +87,23 @@ function getDefendant(row) {
     var Defendant = ""
     if (row.Defendant1 !== "")
         Defendant += row.Defendant1
-    if (row.Defendant2 != "")
+    if (row.Defendant2 !== "")
         Defendant = Defendant + ", " + row.Defendant2
-    if (row.Defendant3 != "")
+    if (row.Defendant3 !== "")
         Defendant = Defendant + ", " + row.Defendant3
-    if (row.Defendant4 != "")
+    if (row.Defendant4 !== "")
         Defendant = Defendant + ", " + row.Defendant4
-    if (row.Defendant5 != "")
+    if (row.Defendant5 !== "")
         Defendant = Defendant + ", " + row.Defendant5
-    if (row.Defendant6 != "")
+    if (row.Defendant6 !== "")
         Defendant = Defendant + ", " + row.Defendant6
-    if (row.Defendant7 != "")
+    if (row.Defendant7 !== "")
         Defendant = Defendant + ", " + row.Defendant7
-    if (row.Defendant8 != "")
+    if (row.Defendant8 !== "")
         Defendant = Defendant + ", " + row.Defendant8
-    if (row.Defendant9 != "")
+    if (row.Defendant9 !== "")
         Defendant = Defendant + ", " + row.Defendant9
-    if (row.Defendant10 != "")
+    if (row.Defendant10 !== "")
         Defendant = Defendant + ", " + row.Defendant10
 
     return Defendant
