a4a2159951f7e8d72a7da8a533cf270a9d2d0ec9
samosbor <samosbor@byu.edu>
2020-03-14 23:09:30 -0600

    commenting

--- client/src/auth/authGuard.js
+++ client/src/auth/authGuard.js
@@ -7,7 +7,7 @@ export const authGuard = async (to, from, next) => {
   console.log($cookies.get("token"))
   // If the user is authenticated, continue with the route
   if ($cookies.get("token")) {
-    if (Object.keys(store.user).length === 0) {
+    if (Object.keys(store.user).length === 0) { // grab the user from db if we dont have it
       await UserService.getUserData($cookies.get("token")).then((result) => {
         console.log(result)
         store.user = result
