f12015a41bba558be82e464403d0c49113535676
Sam Osborne <samosbor@byu.edu>
2020-08-28 20:42:46 -0600

    changed password

--- server/db/db.js
+++ server/db/db.js
@@ -3,7 +3,7 @@ var mysql = require('mysql2');
 var conn = mysql.createConnection({
   host: 'patientpreview01-mysqldbserver.mysql.database.azure.com',
 	user: 'sam@patientpreview01-mysqldbserver',
-	password: '405admin!',
+	password: 'Toastya15(',
 	database: 'production',
 	port: 3306
 });
